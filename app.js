038481ed8ba3a2b4cad0ec29be16ebb8fdc7d84c
donald1623 <donaldaudriel@gmail.com>
2020-01-23 07:06:11 +0100

    added logic to get unix and actual dates

--- app.js
+++ app.js
@@ -9,6 +9,30 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
+//making a get request to date values in json
+app.get('/dateAccess/:dateValue', function(req,res,next){
+    //make request data for date
+    var dateValue = req.params.dateValue;
+    // from javascript to localeDateString docs, to format date output
+    var dateFormatOptions = {
+     year:'numeric',
+     month:'long',
+     day:'numeric'
+    };
+
+     if(isNaN(dateValue)){//when params is a string
+         var actualDate = new Date(dateValue);
+         actualDate = actualDate.toLocaleDateString('en-us', dateFormatOptions);
+         var unixDate = new Date(dateValue).getTime()/1000;
+     } else {//when params is actually a number
+       var unixDate = dateValue;
+       var actualDate = new Date(dateValue * 1000);
+       actualDate = actualDate.toLocaleDateString('en-us', dateFormatOptions);
+     }
+
+    res.json({unix: unixDate, actual: actualDate});
+});
+
 
 app.listen(3000, function(){
   console.log('dtoolz rock!');
